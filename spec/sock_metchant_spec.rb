f25dd47d157184032d6c3a4b30b071a46a7dc1a1
Tonio Serna <antonelo@gmail.com>
2020-09-09 15:48:40 +0200

    Add failing test to count frequency of colors/integers into a hash as key(color/integer), value(frequency)

--- spec/sock_metchant_spec.rb
+++ spec/sock_metchant_spec.rb
@@ -2,17 +2,14 @@ require 'rspec'
 
 socks_array = [1,2,3,1,1,2,2,3,3,2,1,1,1]
 
-def pair_counter(socks_array)
-	socks_array
-end
 
 describe 'Scock Merchant' do
   it 'has to be an array of integers representing the color(1,2,3..n) of each sock' do
     expect(socks_array).to all(be_an(Integer))
   end
 
-  it 'counts pair colors/integers' do
-		expect(pair_counter(socks_array)).to eq(6)
+  it 'counts pair colors/integers into a hash as key(color/integer), value(frequency)' do
+		expect(color_frequency_hash).to eq({1=>6, 2=>4, 3=>3})
   end
 end
 
