f675d00171c3b65de9146704c4483eab0071eac9
Tonio Serna <antonelo@gmail.com>
2020-09-09 15:37:12 +0200

    Add failing test to count pairs of integers/colors inside the array

--- spec/sock_metchant_spec.rb
+++ spec/sock_metchant_spec.rb
@@ -2,12 +2,17 @@ require 'rspec'
 
 socks_array = [1,2,3,1,1,2,2,3,3,2,1,1,1]
 
+def pair_counter(socks_array)
+	socks_array
+end
 
 describe 'Scock Merchant' do
   it 'has to be an array of integers representing the color(1,2,3..n) of each sock' do
     expect(socks_array).to all(be_an(Integer))
   end
 
-
+  it 'counts pair colors/integers' do
+		expect(pair_counter(socks_array)).to eq(6)
+  end
 end
 
