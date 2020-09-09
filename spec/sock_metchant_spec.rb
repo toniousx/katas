dd18d71fd346ec07f5e5d9cc2fb06683478c1b27
Tonio Serna <antonelo@gmail.com>
2020-09-09 15:30:41 +0200

    Make first test passing creating an array of integers defined by color socks

--- spec/sock_metchant_spec.rb
+++ spec/sock_metchant_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 
+socks_array = [1,2,3,1,1,2,2,3,3,2,1,1,1]
+
 
 describe 'Scock Merchant' do
   it 'has to be an array of integers representing the color(1,2,3..n) of each sock' do
